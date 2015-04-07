06973496da00f12881226c152f38152c7f6d7290
Whitney <whitneywooyy@gmail.com>
2015-04-07 16:06:41 -0600

    Completed arrayProperty.js

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,16 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+
+var str = "this is a sentence";
+
+String.prototype.reverse = function() {
+    var backwards = "";
+  		for (var i = this.length - 1; i >= 0; i--) {
+  			backwards = backwards + this[i];
+  		}
+    //console.log(backwards);
+    return backwards;
+};       
+console.log(str.reverse());
