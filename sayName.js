e7b0340e5f153b0553d39af70b11845dc5fe0c91
Whitney <whitneywooyy@gmail.com>
2015-04-07 15:32:04 -0600

    completed sayname.js

--- sayName.js
+++ sayName.js
@@ -2,12 +2,25 @@
 
   //code here
 
+  var Person = function(name, age) {
+  	this.name = name;
+  	this.age = age;
+  };
+
 
 //Now create three instances of Person with data you make up
 
   //code here
 
+var homosapien1 = new Person("Whitney", 25);
+var homosapien2 = new Person("Kyle", 26);
+var homosapien3 = new Person("Chloe is not a person", 6);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+
+Person.prototype.sayName = function() {
+	alert("Hey " + this.name);
+};
+homosapien3.sayName();
